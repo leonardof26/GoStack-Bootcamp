da197aca50faa2cb39881f2a626ef2f19fa0468f
leonardopf26 <leonardopf26@gmail.com>
2019-11-09 11:52:02 -0300

    Added initial authentication process

--- Projects/GoBarber/front-end/src/store/modules/auth/sagas.js
+++ Projects/GoBarber/front-end/src/store/modules/auth/sagas.js
@@ -1,3 +1,38 @@
-import { all } from 'redux-saga/effects'
+import { takeLatest, call, put, all } from 'redux-saga/effects'
 
-export default all([])
+import { signInSuccess } from './actions'
+
+import api from '~/services/api'
+import history from '~/services/history'
+
+export function* signIn({ payload }) {
+  console.tron.error('jdwje')
+  const { email, password } = payload
+  console.tron.error(email)
+  console.tron.error(password)
+
+  try {
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    })
+
+    console.tron.log(response)
+    console.tron.log('fdgds')
+
+    const { token, user } = response.data
+
+    if (!user.provider) {
+      console.tron.error('Only users can logIn')
+      return
+    }
+
+    yield put(signInSuccess(token, user))
+  } catch (error) {
+    console.tron.error(error.response)
+  }
+
+  history.push('/dashboard')
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)])

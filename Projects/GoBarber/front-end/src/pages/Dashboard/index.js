9cdfe52a750d840c4e255d0f39a6f7609f13b2f5
leonardopf26 <leonardopf26@gmail.com>
2019-11-10 09:04:44 -0300

    Added dashboard styles

--- Projects/GoBarber/front-end/src/pages/Dashboard/index.js
+++ Projects/GoBarber/front-end/src/pages/Dashboard/index.js
@@ -1,9 +1,41 @@
 import React from 'react'
+
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 import api from '~/services/api'
 
-// import { Container } from './styles';
+import { Container, Time } from './styles'
 
 export default function Dashboard() {
-  api.get('appointments')
-  return <h1>Dashboard</h1>
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>10 de novembro</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Luke Skywalker</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em Aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Any Skywalker</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Any Skywalker</span>
+        </Time>
+      </ul>
+    </Container>
+  )
 }

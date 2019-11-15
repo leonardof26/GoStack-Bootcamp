9afe947b4202dad8038cb4352aae016a9369bd64
leonardopf26 <leonardopf26@gmail.com>
2019-11-15 15:47:41 -0300

    Added SignIn and SignUp pages layout

--- Projects/GoBarber/mobile/src/pages/SignIn/index.js
+++ Projects/GoBarber/mobile/src/pages/SignIn/index.js
@@ -1,19 +1,46 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Image } from 'react-native'
+
+import logo from '~/assets/logo.png'
 
 import Background from '~/components/Background'
-import TInput from '~/components/Input'
-import Button from '~/components/Button'
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles'
 
-export default function SignIn() {
+export default function SignIn({ navigation }) {
   return (
     <Background>
-      <Text>Ola</Text>
+      <Container>
+        <Image source={logo} />
 
-      <TInput icon="call" placeholder="Digite um telefone" />
-      <Button>Entrar</Button>
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite o seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Criar connta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   )
 }
--- Projects/GoBarber/mobile/src/pages/SignUp/index.js
+++ Projects/GoBarber/mobile/src/pages/SignUp/index.js
@@ -1,14 +1,41 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Image } from 'react-native'
+
+import logo from '~/assets/logo.png'
 
 import Background from '~/components/Background'
 
-// import { Container } from './styles';
+import { Container, Form, FormInput, SubmitButton } from './styles'
 
 export default function SignUp() {
   return (
     <Background>
-      <Text>SignnUp</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome Completo"
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite o seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+      </Container>
     </Background>
   )
 }

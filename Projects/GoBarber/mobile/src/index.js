908ec45ba86cea2133d4038601b63c3b1de32cec
leonardopf26 <leonardopf26@gmail.com>
2019-11-15 17:46:09 -0300

    Added redux connfiguration

--- Projects/GoBarber/mobile/src/index.js
+++ Projects/GoBarber/mobile/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
+
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import './config/reactotronConfig'
+
+import { store, persistor } from './store'
 
 import Routes from './routes'
 
-// import { Container } from './styles';
-
 export default function App() {
-  return <Routes />
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+        <Routes />
+      </PersistGate>
+    </Provider>
+  )
 }
---
+++ Projects/GoBarber/mobile/src/store/modules/auth/sagas.js
@@ -0,0 +1,79 @@
+import { Alert } from 'react-native'
+import { takeLatest, call, put, all } from 'redux-saga/effects'
+
+import { signInSuccess, signFailure } from './actions'
+
+import api from '~/services/api'
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    })
+
+    const { token, user } = response.data
+
+    if (user.provider) {
+      Alert.alert(
+        'Erro no login',
+        'O usuário não pode ser prestador de serviços'
+      )
+      return
+    }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`
+
+    yield put(signInSuccess(token, user))
+
+    // history.push('/dashboard')
+  } catch (error) {
+    Alert.alert(
+      'Falha na autenticação',
+      'Houve um erro no Login, verifique seus dados'
+    )
+    yield put(signFailure())
+  }
+}
+
+export function* signUp({ payload }) {
+  const { name, email, password } = payload
+
+  try {
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    })
+
+    // history.push('/')
+  } catch (error) {
+    Alert.alert(
+      'Erro no cadastro',
+      'Falha na criação de usuário, verifique seus dados'
+    )
+    yield put(signFailure())
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return
+
+  const { token } = payload.auth
+
+  api.defaults.headers.Authorization = `Bearer ${token}`
+}
+
+export function signOut() {
+  // history.push('/')
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
+])

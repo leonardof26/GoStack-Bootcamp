362361b92a1f8fde3cd45b6447eaaa1663756468
leonardopf26 <leonardopf26@gmail.com>
2019-11-16 14:32:04 -0300

    Added profile page

--- Projects/GoBarber/mobile/src/pages/Dashboard/index.js
+++ Projects/GoBarber/mobile/src/pages/Dashboard/index.js
@@ -1,8 +1,58 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState, useEffect } from 'react'
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import Background from '~/components/Background'
+import Appointment from '~/components/Appointment'
+
+import api from '~/services/api'
+
+import { Container, Title, List } from './styles'
 
 export default function Dashboard() {
-  return <View />
+  const [appointmentsList, setAppointmentsList] = useState([])
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('/appointments')
+
+      setAppointmentsList(response.data)
+    }
+
+    loadAppointments()
+  }, [])
+
+  async function handleCancel(id) {
+    const response = await api.delete(`/appointments/${id}`)
+
+    setAppointmentsList(
+      appointmentsList.map(appointment =>
+        appointment.id === id
+          ? { ...appointment, canceled_at: response.data.canceled_at }
+          : appointment
+      )
+    )
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Title> Agendamentos</Title>
+
+        <List
+          data={appointmentsList}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Appointment onCancel={() => handleCancel(item.id)} data={item} />
+          )}
+        />
+      </Container>
+    </Background>
+  )
+}
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamentos',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
 }
--- Projects/GoBarber/mobile/src/routes.js
+++ Projects/GoBarber/mobile/src/routes.js
@@ -5,13 +5,24 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 
 import Dashboard from './pages/Dashboard'
+import Profile from './pages/Profile'
 
 export default (signedIn = false) =>
   createAppContainer(
     createSwitchNavigator(
       {
         Sign: createSwitchNavigator({ SignIn, SignUp }),
-        App: createBottomTabNavigator({ Dashboard }),
+        App: createBottomTabNavigator(
+          { Dashboard, Profile },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#fff',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              style: { backgroundColor: '#5f249f' },
+            },
+          }
+        ),
       },
       { initialRouteName: signedIn ? 'App' : 'Sign' }
     )
